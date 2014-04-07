61441d9d983c7768f05ab9b4deecc984f4da87aa
Stephen J <factorise@gmail.com>
2014-04-08 00:04:23 +0800

    Add CodeCoverage to CircleCI: reverted changes for c3.php

--- environments/dev/frontend/web/index-test.php
+++ environments/dev/frontend/web/index-test.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // NOTE: Make sure this file is not accessible when deployed to production
 if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
@@ -14,7 +13,5 @@ require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/aliases.php');
 
 $config = require(__DIR__ . '/../tests/acceptance/_config.php');
-
-include(__DIR__ . '/c3.php');
 
 (new yii\web\Application($config))->run();
--- frontend/tests/_bootstrap.php
+++ frontend/tests/_bootstrap.php
@@ -4,13 +4,12 @@
 // PLEASE ADJUST IT TO THE ACTUAL ENTRY SCRIPT URL
 //defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/advanced/frontend/web/index-test.php');
 //defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/frontend/web/index-test.php');
-//defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/frontend/web/index-test.php');
-defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/index-test.php');
+defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/frontend/web/index-test.php');
+
 
 // the entry script file path for functional and acceptance tests
 //defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(__DIR__) . '/web/index-test.php');
 defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(__DIR__) . '/web/index-test.php');
-
 
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
@@ -22,9 +21,6 @@ require_once(__DIR__ . '/../../vendor/autoload.php');
 require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 require(__DIR__ . '/../../common/config/aliases.php');
-
-include(__DIR__ . '/../../frontend/web/c3.php');
-
 
 // set correct script paths
 $_SERVER['SCRIPT_FILENAME'] = TEST_ENTRY_FILE;
