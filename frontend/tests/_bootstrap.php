c5b39ecb7c6113a747f8a1b1f2c07513015964c2
Stephen J <factorise@gmail.com>
2014-04-07 23:27:15 +0800

    Add CodeCoverage to CircleCI: moved documentroot to frontend/web. Change the boostrap entry url

--- frontend/tests/_bootstrap.php
+++ frontend/tests/_bootstrap.php
@@ -4,12 +4,13 @@
 // PLEASE ADJUST IT TO THE ACTUAL ENTRY SCRIPT URL
 //defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/advanced/frontend/web/index-test.php');
 //defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/frontend/web/index-test.php');
-defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/frontend/web/index-test.php');
-
+//defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/frontend/web/index-test.php');
+defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/index-test.php');
 
 // the entry script file path for functional and acceptance tests
 //defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(__DIR__) . '/web/index-test.php');
 defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(__DIR__) . '/web/index-test.php');
+
 
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
