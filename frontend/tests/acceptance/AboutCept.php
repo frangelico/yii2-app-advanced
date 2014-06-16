8633bf1a7e1ee9fc46339e5248153758eee5e9ad
Stephen J <factorise@gmail.com>
2014-06-16 23:24:43 +0800

    Add CodeCoverage to CircleCI: cover functional and unit tests

--- frontend/tests/acceptance/AboutCept.php
+++ frontend/tests/acceptance/AboutCept.php
@@ -6,5 +6,17 @@ $I = new WebGuy($scenario);
 $I->wantTo('ensure that about works');
 AboutPage::openBy($I);
 $I->makeScreenshot('open');
+$I-resizeWindow(480,640);
+$I->makeScreenshot('open-480x640');
+$I-resizeWindow(640,960);
+$I->makeScreenshot('open-640x960');
+$I-resizeWindow(960,640);
+$I->makeScreenshot('open-960x640');
+$I-resizeWindow(1024,768);
+$I->makeScreenshot('open-1024x768');
+$I-resizeWindow(1440,900);
+$I->makeScreenshot('open-1440x900');
+$I-resizeWindow(1366,768);
+$I->makeScreenshot('open-1366x768');
 
 $I->see('About', 'h1');
